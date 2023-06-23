b0585e01375758d165b710ca5e758f05428ef4a8
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-23 16:20:32 +0100

    Create LazyComponent2.js

---
+++ LazyComponent2.js
@@ -0,0 +1,14 @@
+//LazyComponent2.js
+
+import React from 'react';
+
+function LazyComponent2() {
+  return (
+    <div>
+      <h2>Lazy Component 2</h2>
+      <p>This is the second lazily loaded component.</p>
+    </div>
+  );
+}
+
+export default LazyComponent2;
